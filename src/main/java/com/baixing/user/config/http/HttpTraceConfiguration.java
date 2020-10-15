31636a164d8722720ae5b06775a1d9b62bfd2793
aowang <wangao@baixing.com>
2020-10-15 11:24:06 +0800

    更新项目说明

---
+++ src/main/java/com/baixing/user/config/http/HttpTraceConfiguration.java
@@ -0,0 +1,35 @@
+package com.baixing.user.config.http;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication.Type;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+
+/**
+ * @author wangao
+ * @date 2018/8/16
+ */
+@Configuration
+@ConditionalOnWebApplication
+public class HttpTraceConfiguration {
+    
+    @ConditionalOnWebApplication(type = Type.SERVLET)
+    static class ServletTraceFilterConfiguration {
+        
+        @Bean
+        @RefreshScope
+        public FilterRegistrationBean httpTraceLogFilter() {
+            FilterRegistrationBean<HttpTraceLogFilter> registration = new FilterRegistrationBean<>();
+            registration.setFilter(new HttpTraceLogFilter());
+            registration.addUrlPatterns("/*");
+            registration.setName("httpTraceLogFilter");
+            registration.setOrder(Ordered.LOWEST_PRECEDENCE - 10);
+            return registration;
+        }
+        
+    }
+    
+}
